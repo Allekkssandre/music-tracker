c53efadde6af7e785d003ddedc6570405c1591aa
Itcodexa <it@codexa.fr>
2026-05-02 22:22:03 +0200

    Add login page and about page

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('login/index.html.twig');
+    }
+
+    #[Route('/login', name: 'app_login_process', methods: ['POST'])]
+    public function process(): Response
+    {
+        return $this->render('login/success.html.twig');
+    }
+}
