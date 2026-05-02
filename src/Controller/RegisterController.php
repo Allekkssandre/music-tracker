bc41543590d418430209089df341289b3be0bde5
Itcodexa <it@codexa.fr>
2026-05-03 00:46:29 +0200

    Users

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,9 +17,8 @@ class RegisterController extends AbstractController
     }
 
 #[Route('/register', name: 'register_process', methods: ['POST'])]
-public function process(Request $request): Response
+public function process(Request $request, EntityManagerInterface $entityManager): Response
 {
-    // Récupérer les données du formulaire
     $name = $request->request->get('name');
     $email = $request->request->get('email');
     $password = $request->request->get('password');
@@ -29,11 +29,24 @@ public function process(Request $request): Response
     $user->setEmail($email);
     $user->setPassword($password);
     
-    // Sauvegarder en DB (pour l'instant, on affiche juste un message)
-    // TODO: on va faire ça
+    // Dire à Doctrine: "prépare cette entité pour la sauvegarde"
+    $entityManager->persist($user);
+    
+    // Exécuter la sauvegarde en DB
+    $entityManager->flush();
     
     return $this->render('register/success.html.twig', [
         'name' => $name
+    ]);
+}
+
+    #[Route('/users', name: 'users_list', methods: ['GET'])]
+    public function listUsers(EntityManagerInterface $entityManager): Response
+{
+    $users = $entityManager->getRepository(User::class)->findAll();
+    
+    return $this->render('users/list.html.twig', [
+        'users' => $users
     ]);
 }
 }
